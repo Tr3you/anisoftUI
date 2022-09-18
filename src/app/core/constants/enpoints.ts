7328d9758286b943c17273eb2a6905adb94027b5
Erick Ruiz <erick.ruiz@capstechnology.co>
2022-09-18 10:51:22 -0500

    .. fix last commit

--- src/app/core/constants/enpoints.ts
+++ src/app/core/constants/enpoints.ts
@@ -1,5 +1,5 @@
 export enum AuthEndpoints {
-    exchange_code = "/auth/exchange",
+    exchange_code = "auth/exchange",
     login = "auth/login",
     signup = "auth/signup",
     confirm = "auth/confirmSignup"
